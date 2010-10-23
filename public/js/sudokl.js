2d68707cd24059bcc6b64834f8e170e29d7f3069
Ross Kaffenberger <rosskaff@gmail.com>
2010-10-23 02:31:01 -0400

    Build sudoku board; add console; style message box

--- public/js/sudokl.js
+++ public/js/sudokl.js
@@ -1,30 +1,72 @@
 Sudokl = (function() {
 
   var instanceMethods = {
-    initialize: function() {
-      this.connect();
+    constructor: function(selector) {
+      this.selector = selector;
+      this.$sudokl  = $(selector);
+
+      this.board    = new Board();
+      this.board.appendTo(this.$sudokl);
+
+      this.$msg     = $("<div>");
+      this.$msg.attr("id", "msg").appendTo(this.$sudokl);
     },
 
     connect: function() {
+      var self = this;
       ws = new WebSocket("ws://localhost:8080/");
-      ws.onmessage = function(evt) { $("#msg").append("<p>"+evt.data+"</p>"); };
-      ws.onclose = function() { debug("socket closed"); };
+      ws.onmessage = function(evt) {
+        self.$msg.append("<p>"+evt.data+"</p>").scrollTop(self.$msg.height());
+      };
+      ws.onclose = function() {
+        console.log("ws:","socket closed");
+      };
       ws.onopen = function() {
-        debug("connected...");
+        console.log("ws:","connected...");
         ws.send("hello server");
       };
+    },
+
+    echo: function() {
+      var self = this;
+      ws = new WebSocket("ws://localhost:8080/");
+      ws.onmessage = function(evt) {
+        self.$msg.append("<p>"+evt.data+"</p>");
+      };
+      ws.onclose = function() { console.log("ws:","socket closed"); };
+      ws.onopen = function() { console.log("ws:","connected..."); ws.send("hello server"); };
     }
   },
 
   classMethods = {
-    begin : function(opts) {
+    play : function(opts) {
       var sudokl = new Sudokl(opts);
       sudokl.connect();
     }
   };
 
+  var Board = Base.extend({
+    constructor: function() {
+      var self = this, $cell, $row;
+      self.$element = $("<div>");
+      self.$element.attr("id", "board").addClass("table");
+      for(var i = 0; i < 9; i++) {
+        $row = $("<div>");
+        $row.addClass("row").attr("id", "row_" + i);
+        self.$element.append($row);
+        for(var j = 0; j < 9; j++) {
+          $cell = $("<div>");
+          $cell.addClass("cell").attr("id","cell_" + i + "_" + j).attr("name", "cell["+i+"]["+j+"]");
+          $row.append($cell);
+        }
+      }
 
-  function debug(str){ $("#debug").append("<p>" +  str); };
+    },
+    appendTo: function($el) {
+      this.$element.appendTo($el);
+    }
+  });
+
 
   return Base.extend(instanceMethods, classMethods);
 })();

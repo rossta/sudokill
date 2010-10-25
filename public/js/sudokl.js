f25696ca55b4227861c6f18d0d417060ee6a9cf0
Ross Kaffenberger <rosskaff@gmail.com>
2010-10-24 23:34:34 -0400

    Draw sudoku board with raphael

--- public/js/sudokl.js
+++ public/js/sudokl.js
@@ -2,11 +2,11 @@ Sudokl = (function() {
 
   var instanceMethods = {
     constructor: function(selector) {
-      this.selector = selector;
+      this.selector = "#" + selector;
       this.$sudokl  = $(selector);
 
-      this.board    = new Board();
-      this.board.appendTo(this.$sudokl);
+      this.board    = new SuBoard(selector);
+      this.board.build();
 
       this.$msg     = $("<div>");
       this.$msg.attr("id", "msg").appendTo(this.$sudokl);
@@ -45,28 +45,75 @@ Sudokl = (function() {
     }
   };
 
-  var Board = Base.extend({
-    constructor: function() {
-      var self = this, $cell, $row;
-      self.$element = $("<div>");
-      self.$element.attr("id", "board").addClass("table");
-      for(var i = 0; i < 9; i++) {
-        $row = $("<div>");
-        $row.addClass("row").attr("id", "row_" + i);
-        self.$element.append($row);
-        for(var j = 0; j < 9; j++) {
-          $cell = $("<div>");
-          $cell.addClass("cell").attr("id","cell_" + i + "_" + j).attr("name", "cell["+i+"]["+j+"]");
-          $row.append($cell);
+  var SuBoard = Base.extend({
+    constructor: function(selector) {
+      this.r = Raphael(selector, 450, 450);
+      this.dim = 50;
+      this.entries = [];
+    },
+    build: function() {
+      var r = this.r,
+      groups  = r.set(),
+      squares = r.set(),
+      dim = 50,
+      gDim = 3 * dim,
+      start = 0,
+      strokeColor = 'green',
+      color = "hsb(" + start + ", 1, .5)",
+      bcolor = "hsb(" + start + ", 1, 1)",
+      group, square, text, x, y, cx, cy;
+
+      r.rect(0, 0, gDim * 3, gDim * 3).attr({
+        stroke: strokeColor,
+        "stroke-opacity": 0.5,
+        "stroke-width": 4
+      });
+
+      for(var i = 0; i < 3; i++) {
+        y = i * gDim;
+        for(var j = 0; j < 3; j++) {
+          x = j * gDim;
+          group = r.rect(x, y, gDim, gDim);
+          groups.push(group);
         }
       }
 
-    },
-    appendTo: function($el) {
-      this.$element.appendTo($el);
-    }
-  });
+      for(var i = 0; i < 9; i++) {
+        y = i * dim;
+        for(var j = 0; j < 9; j++) {
+          color = "hsb(" + start + ", 1, .5)";
+          bcolor = "hsb(" + start + ", 1, 1)";
+          x = j * dim;
+          cx = x + (dim/2);
+          cy = y + (dim/2);
+          
+          square = r.rect(x, y, dim, dim);
+          
+          text = r.text(cx, cy, Math.floor(Math.random()*9) + 1).attr({
+            fill: bcolor,
+            "text-anchor": "middle",
+            "font-size": "32px",
+            "color": "white"
+          });
 
+          squares.push(square);
+          start += 0.01;
+        }
+      }
+
+      squares.attr({
+        stroke: strokeColor,
+        "stroke-opacity": 0.5,
+        "stroke-width": 1
+      });
+
+      groups.attr({
+        stroke: strokeColor,
+        "stroke-opacity": 0.5,
+        "stroke-width": 2
+      });
+    }
+  },{});
 
   return Base.extend(instanceMethods, classMethods);
 })();
